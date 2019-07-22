7e782d72a2b6cb8de00384ba5db1dd41ac69876a
Daniel Munoz <daniel.munozdev@gmail.com>
2019-07-21 22:36:13 -0700

    Updates

---
+++ precache-manifest.f38ab079987e412b298a2131fa5dfecb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b8d6a2e662fdb21dc46be51cc05d35",
+    "url": "/quill-change-placeholder/index.html"
+  },
+  {
+    "revision": "dc47631517b0a83e0672",
+    "url": "/quill-change-placeholder/static/css/2.4b3ccb6c.chunk.css"
+  },
+  {
+    "revision": "3b66aa2d422c7da30c4a",
+    "url": "/quill-change-placeholder/static/css/main.a517162b.chunk.css"
+  },
+  {
+    "revision": "dc47631517b0a83e0672",
+    "url": "/quill-change-placeholder/static/js/2.1e3fc2c0.chunk.js"
+  },
+  {
+    "revision": "3b66aa2d422c7da30c4a",
+    "url": "/quill-change-placeholder/static/js/main.e2cbc85d.chunk.js"
+  },
+  {
+    "revision": "f581e0958dbf67aa169b",
+    "url": "/quill-change-placeholder/static/js/runtime~main.f3936925.js"
+  }
+]);
